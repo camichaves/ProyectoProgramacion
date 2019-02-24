ce9efd842352d9cdbef86ed1cc9cebd46ca92f98
Camila <camichavesthg@gmail.com>
2019-02-24 10:43:55 -0300

    API

--- chofer.php
+++ chofer.php
@@ -10,7 +10,7 @@ $conexion = new PDO("mysql:host=$servidor;dbname=$bd",$usuario,$clave);
 
 
 
-$hora = date('j-G.d');
+$hora = date('j-G');
 
 $token = hash('sha256', $hora);
 
@@ -42,7 +42,7 @@ $token = hash('sha256', $hora);
                     $res = $ejecucionSQL->fetch(PDO::FETCH_ASSOC);
                     echo json_encode($res);
                 } else {
-                    $sql = "SELECT chofer.`chofer_id`,chofer.`apellido`,chofer.`nombre`,chofer.`documento`,chofer.`email`,marca,sistema_transporte.`nombresistema` FROM chofer INNER JOIN vehiculo ON chofer.vehiculo_id = vehiculo.vehiculo_id INNER JOIN sistema_transporte `";
+                    $sql = "SELECT chofer.`chofer_id`,chofer.`apellido`,chofer.`nombre`,chofer.`documento`,chofer.`email`,marca,sistema_transporte.`nombresistema` FROM chofer INNER JOIN vehiculo ON chofer.vehiculo_id = vehiculo.vehiculo_id INNER JOIN sistema_transporte ON chofer.sistema_id = sistema_transporte.`sistema_id`";
                     $stmt = $conexion->prepare($sql);
                     $stmt->execute();
                     $choferes = array();
@@ -78,7 +78,16 @@ $token = hash('sha256', $hora);
                     $ejecucionSQL = $conexion->prepare($sql);
                     $ejecucionSQL->execute();
                 }
-                
+                if (isset($data["email"])) {
+                    $sql = "UPDATE `chofer` SET `email` = '" . $data["email"] . "' WHERE `chofer_id` = '" . $data["chofer_id"] . "';";
+                    $ejecucionSQL = $conexion->prepare($sql);
+                    $ejecucionSQL->execute();
+                }
+                if (isset($data["vehiculo_id"])) {
+                    $sql = "UPDATE `chofer` SET `vehiculo_id` = '" . $data["vehiculo_id"] . "' WHERE `chofer_id` = '" . $data["chofer_id"] . "';";
+                    $ejecucionSQL = $conexion->prepare($sql);
+                    $ejecucionSQL->execute();
+                }
                 if (isset($data["sistema_id"])) {
                     $sql = "UPDATE `chofer` SET `sistema_id` = '" . $data["sistema_id"] . "' WHERE `chofer_id` = '" . $data["chofer_id"] . "';";
                     $ejecucionSQL = $conexion->prepare($sql);
@@ -95,6 +104,16 @@ $token = hash('sha256', $hora);
         case "DELETE":
             $data = json_decode(file_get_contents("php://input"),true);
             if($token==$data["token"]) {
+                $time_pre = microtime(true);
+                $sql = "DELETE FROM `chofer` WHERE `chofer_id` = '" . $data["chofer_id"] . "';";
+                $ejecucionSQL = $conexion->prepare($sql);
+                $ejecucionSQL->execute();
+                $time_post = microtime(true);
+                $time = $time_post - $time_pre;
+                $time = $time*pow(10,3);
+                $sql = "INSERT INTO `auditoria` (`fecha_acceso`, `user`, `response_time`, `endpoint`) VALUES ('" . date('Y-m-d H:i:s') . "', '" . $data["usuario"] . "', '" . $time . "', 'borrarChofer');";
+                $ejecucionSQL = $conexion->prepare($sql);
+                $ejecucionSQL->execute();
             }
             break;
     }
